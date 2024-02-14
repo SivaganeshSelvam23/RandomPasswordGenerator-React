5f0f9f4cbbf4a315a04637401c560677b13b68cd
SivaganeshSelvam23 <sivaganesh.selvam.2324@gmail.com>
2024-02-14 13:07:46 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import usePasswordGenerator from "./hooks/usePasswordGenerator";
+import StrengthChecker from "./component/StrengthChecker";
 function App() {
+  const [length, setLength] = useState(4);
+  const [checkboxData, setCheckboxData] = useState([
+    { title: "Include Uppercase Letters", state: false },
+    { title: "Include Lowercase Letters", state: false },
+    { title: "Include Numbers", state: false },
+    { title: "Include Symbols", state: false },
+  ]);
+  const [copied, setCopied] = useState(false);
+  const handleCheckboxChange = (i) => {
+    const updatedCheckboxData = [...checkboxData];
+    updatedCheckboxData[i].state = !updatedCheckboxData[i].state;
+    setCheckboxData(updatedCheckboxData);
+  };
+  console.log("checkboxData:", checkboxData);
+  const { password, errorMessage, generatePassword } = usePasswordGenerator();
+  const handelCopy = (password) => {
+    navigator.clipboard.writeText(password);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* Password text copy button */}
+      {password && (
+        <div className="header">
+          <div className="title">{password}</div>
+          <button className="copyBtn" onClick={() => handelCopy(password)}>
+            {copied ? "Copied" : "copy"}
+          </button>
+        </div>
+      )}
+
+      {/* Charater length */}
+      <div className="charlength">
+        <span>
+          <label>Charater Length</label>
+          <label>{length}</label>
+        </span>
+        <input
+          type="range"
+          min="4"
+          max="20"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+        />
+      </div>
+      {/* CheckBox */}
+      <div className="checkboxes">
+        {checkboxData.map((checkbox, index) => {
+          return (
+            <div key={index}>
+              <input
+                type="checkbox"
+                checked={checkbox.state}
+                onChange={() => handleCheckboxChange(index)}
+              />
+              <label>{checkbox.title}</label>
+            </div>
+          );
+        })}
+      </div>
+      {/* strength */}
+      <StrengthChecker password={password} />
+      {/* Error Handling */}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+      {/* generate button */}
+      <button
+        className="generateBtn"
+        onClick={() => generatePassword(checkboxData, length)}
+      >
+        Generate Password
+      </button>
     </div>
   );
 }
---
+++ src/component/StrengthChecker.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const StrengthChecker = ({ password }) => {
+  const getPasswordStrength = () => {
+    const passwordLength = password.length;
+    if (passwordLength < 1) {
+      return "";
+    } else if (passwordLength < 4) {
+      return "Very Weak";
+    } else if (passwordLength < 8) {
+      return "Poor";
+    } else if (passwordLength < 12) {
+      return "Medium";
+    } else if (passwordLength < 16) {
+      return "Strong";
+    } else {
+      return "Very Strong";
+    }
+  };
+
+  const passwordStrength = getPasswordStrength();
+  if (!passwordStrength) return <></>;
+  return (
+    <div className="password-strength">
+      Strength: <span style={{ fontWeight: "bold" }}>{passwordStrength}</span>
+    </div>
+  );
+};
+
+export default StrengthChecker;
